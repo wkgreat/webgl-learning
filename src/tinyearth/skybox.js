5175198736df901fcc8bc3b992f1682eb7bd7df1
ke.wang <ke.wang@transwarp.io>
2025-06-27 22:08:59 +0800

    skybox picture compress

--- src/tinyearth/skybox.js
+++ src/tinyearth/skybox.js
@@ -154,7 +154,7 @@ export class SkyBoxProgram {
             img.src = face.src;
             that.gl.bindTexture(that.gl.TEXTURE_CUBE_MAP, that.#texutre);
             that.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, false);
-            that.gl.texImage2D(face.face, 0, that.gl.RGBA, 2048, 2048, 0, that.gl.RGBA, that.gl.UNSIGNED_BYTE, null); //立即渲染纹理
+            that.gl.texImage2D(face.face, 0, that.gl.RGBA, 512, 512, 0, that.gl.RGBA, that.gl.UNSIGNED_BYTE, null); //立即渲染纹理
             img.onload = function () {
                 // 图片加载完成将其拷贝到纹理
                 that.gl.bindTexture(that.gl.TEXTURE_CUBE_MAP, that.#texutre);
